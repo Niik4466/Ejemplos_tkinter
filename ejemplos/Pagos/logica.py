ffad6bae533a40980e2a7ff44b2bc45f7f2b72c2
niik4466 <niikk4466@gmail.com>
2026-05-14 17:14:29 -0400

    hola

--- ejemplos/Pagos/logica.py
+++ ejemplos/Pagos/logica.py
@@ -1,6 +1,6 @@
 from datos import guardar_pago, obtener_pagos
 
-def registrar_pago(rut:str, fecha:str, monto:str, lugar:str):
+def registrar_pago(rut:str, fecha:str, monto:str, lugar:str) -> tuple[bool, any]:
     """
     Valida los datos y registra un nuevo pago.
     Retorna una tupla (booleano, mensaje) indicando éxito o error.
